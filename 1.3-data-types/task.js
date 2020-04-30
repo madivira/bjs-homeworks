b2ccf2c18fd34a3c885f5cc02979966c476b13bd
Diana Maslyakova <diana_777@mail.ru>
2020-04-30 11:35:50 +0300

    Homework 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,37 @@
+
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    if (isNaN(parseInt(percent))) { 
+        return `Параметр процентная ставка содержит неправильное значение`;
+    }
+    if (isNaN(parseInt(contribution))) {
+        return `Параметр первоначальный взнос содержит неправильное значение`;
+    }
+    if (isNaN(parseInt(amount))) {
+        return `Параметр общая стоимость содержит неправильное значение`;
+    }
+
+    let S = Number(amount) - Number(contribution);
+    console.log(S);
+    let P = (Number(percent) / 100) / 12;
+    console.log(P);
+    let n = (date.getFullYear() - new Date().getFullYear()) * 12;
+    console.log(n);
+    let monthlyPayment = S * ( P + P / ( ( ( 1 +  P )**n) - 1 ) );
+    console.log(monthlyPayment);
+    let totalAmount = monthlyPayment * n;
+
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
+     
+    if (!String(name) || name >=0 || name === undefined)  {
+        return `Привет, мир! Меня зовут Аноним`;
+    }  else {
+        return `Привет, мир! Меня зовут ${name}`;
+    }
     // return greeting;
 }
